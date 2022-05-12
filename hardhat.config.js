37041714ca5bbc4c22fd43a74263cb551371cf25
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-05-12 09:08:39 -0400

    added networks and set defaultNetwork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy");
-require('@nomiclabs/hardhat-etherscan');
-require('dotenv').config();
-
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: process.env.INFURA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   namedAccounts: {
     deployer: {
-      default: 0
-    }
-  }
+      default: 0,
+    },
+  },
 };
